ab8baf8fce8c1f78e926325d9d7940910226fbe0
leijiwu <1287279970@qq.com>
2019-06-25 23:51:34 +0800

    写了新增

--- models/user.go
+++ models/user.go
@@ -1,6 +1,11 @@
 package models
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 type User struct {
 	Id       int64  `orm:"auto"`
@@ -8,12 +13,29 @@ type User struct {
 	Nickname string `orm:"size(100)"`
 }
 
-func Creat(name string, nickname string) (user User) {
+func init() {
+	// 需要在init中注册定义的model
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", "root:123456@tcp(localhost:3306)/beego_leaning?charset=utf8")
+	orm.RegisterModel(new(User))
+}
 
+func Creat(name string, nickname string) (user User) {
+	o := orm.NewOrm()
+	o.Using("default") // 默认使用 default，你可以指定为其他数据库
 	fmt.Println("models被调用到了")
 	newuser := new(User)
 	newuser.Id = 1
 	newuser.Name = "这是名称"
 	newuser.Nickname = "这是昵称"
+	o.Insert(newuser)
+	return *newuser
+}
+
+func GetUser() (user User) {
+	o := orm.NewOrm()
+	o.Using("default") // 默认使用 default，你可以指定为其他数据库
+	fmt.Println("models被调用到了")
+	qs := o.QueryTable("user")
 	return *newuser
 }
